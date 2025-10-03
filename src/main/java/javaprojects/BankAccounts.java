010e87036d7df9a585e897389c6dfb23f390495a
kishan955971 <kishan221314z@gmail.com>
2025-10-03 12:58:40 +0530

    project update

--- src/main/java/javaprojects/BankAccounts.java
+++ src/main/java/javaprojects/BankAccounts.java
@@ -2,13 +2,16 @@ package javaprojects;
 
 import java.io.Serializable;
 import java.util.Scanner;
+
 public class BankAccounts implements Serializable {
+
     public static final long serialVersionUID = 1;
     private Person customer;
     private int balance;
     private String account;
+
     public BankAccounts() throws InvalidAgeException, InvalidMobileNumberException, NegativeNumberException,
-        NumberFormatException {
+            NumberFormatException {
         Scanner s = new Scanner(System.in);
         this.customer = new Person();
         System.out.print("Enter the account number : ");
@@ -18,6 +21,7 @@ public class BankAccounts implements Serializable {
         Validations.isInvalidAmount(balance);
         this.balance = balance;
     }
+
     public void deposit() throws NegativeNumberException, NumberFormatException {
         Scanner s = new Scanner(System.in);
         System.out.print("Enter the amount to deposit : ");
@@ -25,6 +29,7 @@ public class BankAccounts implements Serializable {
         Validations.isInvalidAmount(amount);
         this.balance += amount;
     }
+
     public void withdraw() throws NegativeNumberException, NumberFormatException, InsufficientAmountException {
         Scanner s = new Scanner(System.in);
         System.out.print("Enter the amount to withdraw : ");
@@ -35,16 +40,20 @@ public class BankAccounts implements Serializable {
         }
         this.balance -= amount;
     }
+
     @Override
     public String toString() {
         return "BankAccount{" + "customer=" + customer + ", balance=" + balance + ", accountno=" + account + '}';
     }
+
     public Person getCustomer() {
         return customer;
     }
+
     public int getBalance() {
         return balance;
     }
+
     public String getAccountno() {
         return account;
     }
--- src/main/java/javaprojects/Utilities.java
+++ src/main/java/javaprojects/Utilities.java
@@ -3,20 +3,26 @@ package javaprojects;
 import java.io.*;
 import java.util.Map;
 import java.util.TreeMap;
+
 public class Utilities {
+
     private static Map<String, BankAccounts> accounts;
+
     public static void writeAccounts() throws IOException {
         ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream("data.txt"));
         out.writeObject(accounts);
         out.flush();
         out.close();
     }
+
     public static Map<String, BankAccounts> getAccounts() {
         return accounts;
     }
+
     public static String getNewAccountNo() {
         return null;
     }
+
     public static void readAccounts() throws ClassNotFoundException {
         try {
             ObjectInputStream in = new ObjectInputStream(new FileInputStream("data.txt"));
@@ -27,6 +33,7 @@ public class Utilities {
             accounts = new TreeMap<String, BankAccounts>();
         }
     }
+
     public static BankAccounts getBankAccount(String accountnumber) throws AccountNotFoundException {
         BankAccounts account = accounts.get(accountnumber);
         if (account == null) {
